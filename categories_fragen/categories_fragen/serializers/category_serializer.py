d43887dee8fa75b5a466a8b829ca0904562b68a4
SantiMahecha <smahechap@unal.edu.co>
2020-10-08 00:28:47 -0500

    Update category_serializer.py

--- categories_fragen/categories_fragen/serializers/category_serializer.py
+++ categories_fragen/categories_fragen/serializers/category_serializer.py
@@ -4,5 +4,5 @@ from categories_fragen.models.category_model import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = ['id', 'name','description']
 
