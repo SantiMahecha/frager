e011d66e975876d14ee7bf4216550e0f21ad48e0
SantiMahecha <smahechap@unal.edu.co>
2020-10-07 21:04:44 -0500

    Update urls.py

--- categories_fragen/categories_fragen/urls.py
+++ categories_fragen/categories_fragen/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from frager.views.category_view import CategoryList
-from frager.views.category_view import CategoryDetail
+from categories_fragen.views.category_view import CategoryList
+from categories_fragen.views.category_view import CategoryDetail
 
 urlpatterns = [
     path('categories/', CategoryList.as_view()),
